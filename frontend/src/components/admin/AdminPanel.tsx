2b549e7740258ae1d3f9f9fe7368e71be3a3b170
icambraa <126367844+icambraa@users.noreply.github.com>
2024-05-25 20:13:39 +0200

    Siome changes

--- frontend/src/components/admin/AdminPanel.tsx
+++ frontend/src/components/admin/AdminPanel.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Card, Container, Row, Col, ListGroup } from 'react-bootstrap';
 
 const AdminPanel: React.FC = () => {
     return (
-        <div>
-            <h1>Panel de Administrador</h1>
-            <ul>
-                <li>
-                    <Link to="/admin-panel/reports">Ver reportes</Link>
-                </li>
-            </ul>
-        </div>
+        <Container style={{ marginTop: '150px' }}>
+            <Row className="justify-content-center">
+                <Col md={8}>
+                    <Card>
+                        <Card.Header className="text-center">
+                            <h1>Panel de Administrador</h1>
+                        </Card.Header>
+                        <Card.Body>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>
+                                    <Link to="/admin-panel/reports">Ver reportes</Link>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
